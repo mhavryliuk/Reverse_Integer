e952ac5ace9fa3b5c89875b1c70f2d07a8129138
Maksym Havryliuk <maks.havryliuk@gmail.com>
2018-03-26 17:12:16 +0300

    Addition of the solution of a problem of Reverse Integer.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/* https://leetcode.com/problems/reverse-integer/description/
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ * Assume we are dealing with an environment which could only hold integers within the 32-bit signed integer range. 
+ * For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows. */
+
+namespace _00007_ReverseInteger
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Input your integer number: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Solution answer = new Solution();
+            Console.WriteLine("Answer: " + answer.Reverse(x));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _00007_ReverseInteger
+{
+    class Solution
+    {
+        public int Reverse(int x)
+        {
+            int answer = 0;
+            while (x != 0)
+            {
+                x = Math.DivRem(x, 10, out int remainder);
+                try
+                {
+                    answer = checked(answer = answer * 10 + remainder);
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+            return answer;
+        }
+    }
+}
